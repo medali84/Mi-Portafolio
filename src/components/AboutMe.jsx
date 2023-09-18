624aaf92f015e376d97319738365f4127b6970b8
Mohamed Ali <meh.4891@gmail.com>
2023-09-18 08:23:38 +0200

    actualizacion

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,7 +16,9 @@ export const AboutMe = () => {
             <div className="aboutme-datos">
             <h2 className="aboutme-titulo">Sobre mi</h2>
             <p className="aboutme-texto">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>  
+            <a className="descargar-cv" href="public\imgs\Mohamed Ali.pdf" download>
             <button>DESCARGA C.V.</button>
+          </a>
        
         </div>
           </div>
