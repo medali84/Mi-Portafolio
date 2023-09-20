f749fb325fb3d93392f04e7a73cbac447d549a69
Mohamed Ali <meh.4891@gmail.com>
2023-09-20 08:42:00 +0200

    enlazar con Vercel

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+/* import { useState } from 'react';
 import CreateProject from '../components/CreateProject';
 import EditProject from '../components/EditProject';
 import "./MainComponent.css"
@@ -32,4 +32,4 @@ const MainComponent = () => {
     );
 }
 
-export default MainComponent;
+export default MainComponent; */
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom';
 
-import { RouterProvider } from 'react-router-dom'
+
+import { BrowserRouter as RouterProvider } from 'react-router-dom';
+
 import { router } from './router'
 
 
